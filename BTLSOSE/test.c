20fb32ce0a06b693c8a4e29285002115ed315e66
thanhbinh20239668 <dothanhbinhntt2005@gmail.com>
2026-03-23 22:06:09 +0700

    delete file rac

--- BTLSOSE/test.c
+++
@@ -1,8 +0,0 @@
-#include "serpent.h"
-char XORchar1(char a, char b) { return a ^ b; }
-int main() {
-  char a = 'A';
-  char b = 0x20;
-  printf("%c\n", XORchar1(a, XORchar1(a, b)));
-  return 0;
-}
